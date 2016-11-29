bd8156d29ef8e200a9f457c4dbaac54af6ce988c
Sharon Zhang <zhangshiyu1992@hotmail.com>
2016-11-29 14:53:37 +0800

    Update index.php

--- index.php
+++ index.php
@@ -1,27 +1,26 @@
-<?php
-get_header(); 
-?>
+<?php get_header();?>
  <?php 
  $option=get_option('erlsimple_theme_options');
  if($option['if_bg_on']==1):
  ?>
- <style>
+<style>
 .nav-bar{ display:none;}
 </style>
 <header class="site-header-container">
 	<div class="site-header">
 		<div class="site-header-inner">
 			<a href="<?php bloginfo('siteurl'); ?>" rel="home" >
-			<h1>
-			<?php if($option['homelogo']):?>
-			<img src="<?php echo $option['homelogo'];?>" id="logo" class="animated fadeInDown" />
-			<?php elseif($option['logoimg']):?>
-			<img src="<?php echo $option['logoimg'];?>" id="logo" class="animated fadeInDown" />
-			<?php else:
-				echo get_bloginfo('name');
-				endif;
-			?>
-			</h1></a>
+				<h1>
+				<?php if($option['homelogo']):?>
+					<img src="<?php echo $option['homelogo'];?>" id="logo" class="animated fadeInDown" />
+				<?php elseif($option['logoimg']):?>
+					<img src="<?php echo $option['logoimg'];?>" id="logo" class="animated fadeInDown" />
+				<?php else:
+					echo get_bloginfo('name');
+					endif;
+				?>
+				</h1>
+			</a>
 			<h2 class="animated fadeInUp"><?php echo get_bloginfo('description');?></h2>
 		</div>
 	</div>
@@ -30,8 +29,8 @@ get_header();
 <div class="container">
 	<div class="main">
 		<div class="content">
-				<div class="posts-inner">
 			<section class="posts">
+				<div class="posts-inner">
 					<?php 
 					if (have_posts()) :
 						while (have_posts()) : the_post();
@@ -41,7 +40,7 @@ get_header();
 					else:?>
 					<h3 style="margin-top:45px; text-align:center"><i class="fa fa-exclamation-triangle"></i>&nbsp;&nbsp;暂无文章发表！</h3>
 					<?php endif;?>
-				</div><!-- /.posts-inner -->
+				</div>
 			</section>
 		</div>
 	<?php get_sidebar(); ?>
