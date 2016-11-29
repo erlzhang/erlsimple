b0803801029f8205b26192c51967ec1ca3124bc5
Sharon Zhang <zhangshiyu1992@hotmail.com>
2016-11-29 15:13:52 +0800

    Update page.php

--- page.php
+++ page.php
@@ -1,14 +1,14 @@
 <?php get_header(); ?>
-<?php if (have_posts()) : ?> 
-   <?php while (have_posts()) : the_post(); ?>
-		<?php if ( has_post_thumbnail() ) : ?> 
+<?php if (have_posts()) :?> 
+   <?php while (have_posts()) : the_post();?>
+		<?php if ( has_post_thumbnail() ) :?> 
 		<style>
-		/*.nav-bar{ display:none;}*/
+		.nav-bar{ display:none;}
 		</style>
         <header class="site-header" style=" height:30%;">
 			<div class="site-header-inner" style="animation: bgcolor 5s;-moz-animation: bgcolor 5s;-webkit-animation: bgcolor 5s;	-o-animation: bgcolor 5s;background-color: rgba(0,0,0,0.3);">
-				<h1 style=" color:#fff;" class="animated fadeInDown"><?php the_title(); ?></h1>
-			</div><!-- /.site-header-inner -->
+				<h1 style=" color:#fff;" class="animated fadeInDown"><?php the_title();?></h1>
+			</div>
 		</header>
 		<?php endif;?>
 	<div class="main">
@@ -18,17 +18,16 @@
 			   <?php if(function_exists('cmp_breadcrumbs')) cmp_breadcrumbs();?></div>
 				<div class="posts-inner">
 					<article class="post text">
-						<?php if(!has_post_thumbnail()):?><h1 itemprop="name"><?php the_title(); ?></h1>
+						<?php if(!has_post_thumbnail()):?><h1 itemprop="name"><?php the_title();?></h1>
 						<?php endif;?>
-						<?php the_content(); ?>                
+						<?php the_content();?>                
 					</article>
-				</div><!-- /.posts-inner -->
-				<div class="post-comment">
-					<?php comments_template(); ?>
 				</div>
-			</section><!-- /.posts -->
+				<div class="post-comment">
+					<?php comments_template();?>
+				</div>
+			</section>
 		</div>
 	</div>
-    <?php endwhile; ?>
-<?php endif; ?> 
-<?php get_footer(); ?>
+    <?php endwhile; endif;?>
+<?php get_footer();?>
