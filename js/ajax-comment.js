0409962e02fbc495b55390878ac4c27f37424fbf
Sharon Zhang <zhangshiyu1992@hotmail.com>
2016-11-29 12:53:55 +0800

    ajax 评论

---
+++ js/ajax-comment.js
@@ -0,0 +1,100 @@
+﻿/*
+AJAX COMMENT
+Author: Erl
+Author URI: https://github.com/erlzhang
+*/
+
+var respond = $("#respond");
+
+function cancelReply(){
+	respond.insertAfter("#respond_box");
+	parent.value = "0";
+	this.style.display = "none";
+	this.onclick = null;
+	return false	
+}
+
+var changeMsg = "[ 更改 ]";
+var closeMsg = "[ 隐藏 ]";
+function toggleCommentAuthorInfo() {
+	jQuery('#comment-author-info').slideToggle('slow', function(){
+		if ( jQuery('#comment-author-info').css('display') == 'none' ) {
+		jQuery('#toggle-comment-author-info').text(changeMsg);
+		} else {
+		jQuery('#toggle-comment-author-info').text(closeMsg);
+}
+});
+}
+jQuery(document).ready(function(){
+	jQuery('#comment-author-info').hide();
+});
+	
+addComment = {
+	moveForm: function(commId, parentId, respondId, postId, num){
+		var reply = document.getElementById("replying"),
+			parent = document.getElementById("comment_parent");
+		event.preventDefault();
+		respond.insertAfter("#"+commId);
+		$(reply).show()
+		$("#replying-parent").html($("#"+commId).find(".fn").html())
+		parent.value = parentId;
+		reply.onclick = cancelReply;
+	}
+}
+$("#commentform").submit(function(){
+	event.preventDefault();
+	$("#replying").hide();
+	$("#loading").show();
+	$("#submit").attr("disabled",true).fadeTo("slow",.5);
+	$.ajax({
+		url : Myajax.ajaxurl,
+		data : $(this).serialize() + '&action=submit_comment',
+		type: $(this).attr("method"),
+		error:function(request){
+			$("#error").html("<p><strong>错误：</strong>服务器通讯失败，请稍后重试。</p>").show();
+		},
+		success:function(data){
+			console.log(data);
+			$("#loading").hide();
+			var div = document.createElement("div");
+			div.innerHTML = data;
+			if (data == "0"){
+				$("#error").html("<p><strong>错误：</strong>服务器通讯失败，请稍后重试。</p>").show();
+			}
+			if( $(div).find("#comment-error").length > 0){
+				//评论提交失败
+				$("#error").html(data).show();
+			}else{
+				//评论提交成功
+				$("#success").show();
+				var p = $(data).data("parent");
+				var container;
+				console.log(container);
+				console.log(p);
+				if(p == 0){
+					container = $(".comment-list");
+					
+				}else{
+					cancelReply();
+					if($("#comment-" + p).find("ul.children").length > 0){
+						container = $("#comment-" + p).find("ul.children")
+					}else{
+						container = document.createElement("ul");
+						container = $(container);
+						container.addClass("children").hide();
+						$("#comment-" + p).append(container);
+						container.fadeIn();
+					}
+				}
+				container.prepend(data);
+				$('html,body').animate({
+					scrollTop :$(container).offset().top - 100
+				})
+			}
+			setTimeout(function(){
+				$("#success,#error").fadeOut(1000);
+				$("#submit").attr("disabled",false).fadeTo("slow",1);
+			},1500);
+		}
+	});
+});
